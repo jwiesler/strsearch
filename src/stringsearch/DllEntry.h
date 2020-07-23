cee43d11275210107ea61068309dea18a55085ea
Julian Wiesler <wiesleju@gmail.com>
2020-07-24 00:56:16 +0200

    Calling convention

--- src/stringsearch/DllEntry.h
+++ src/stringsearch/DllEntry.h
@@ -15,18 +15,20 @@ struct FindUniqueItemsResult {
 	size_t Consumed;
 };
 
+#define strsearchdll_CALLING_CONVENCTION __cdecl
+
 extern "C" {
 	using InstanceHandle = void *;
 	
-	strsearchdll_EXPORT void SuffixSortSharedBuffer(const char16_t *characters, stringsearch::Index *saBegin, stringsearch::Index *saEnd);
+	strsearchdll_EXPORT void strsearchdll_CALLING_CONVENCTION SuffixSortSharedBuffer(const char16_t *characters, stringsearch::Index *saBegin, stringsearch::Index *saEnd);
 
-	strsearchdll_EXPORT void SuffixSortInPlace(const char16_t *characters, stringsearch::Index *saBegin, stringsearch::Index *saEnd);
+	strsearchdll_EXPORT void strsearchdll_CALLING_CONVENCTION SuffixSortInPlace(const char16_t *characters, stringsearch::Index *saBegin, stringsearch::Index *saEnd);
 
-	strsearchdll_EXPORT void *CreateSearchInstanceFromText(const char16_t *charactersBegin, size_t count);
+	strsearchdll_EXPORT void * strsearchdll_CALLING_CONVENCTION CreateSearchInstanceFromText(const char16_t *charactersBegin, size_t count);
 	
-	strsearchdll_EXPORT void DestroySearchInstance(InstanceHandle instance);
+	strsearchdll_EXPORT void strsearchdll_CALLING_CONVENCTION DestroySearchInstance(InstanceHandle instance);
 
-	strsearchdll_EXPORT Result CountOccurences(InstanceHandle instance, const char16_t *patternBegin, size_t count, int *occurrences);
+	strsearchdll_EXPORT Result strsearchdll_CALLING_CONVENCTION CountOccurences(InstanceHandle instance, const char16_t *patternBegin, size_t count, int *occurrences);
 	
-	strsearchdll_EXPORT Result FindUniqueItems(InstanceHandle instance, const char16_t *patternBegin, size_t count, stringsearch::Index *output, size_t outputCount, FindUniqueItemsResult *result, unsigned int offset);
+	strsearchdll_EXPORT Result strsearchdll_CALLING_CONVENCTION FindUniqueItems(InstanceHandle instance, const char16_t *patternBegin, size_t count, stringsearch::Index *output, size_t outputCount, FindUniqueItemsResult *result, unsigned int offset);
 }
